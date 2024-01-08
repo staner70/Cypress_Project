be6020496f330117953088bc251c644d6327a55f
staner70 <staner2021@gmail.com>
2024-01-08 11:05:49 +0100

    Refactoring LoginTest and LogoutUserTest

--- cypress/e2e/Test/LoginTest.cy.js
+++ cypress/e2e/Test/LoginTest.cy.js
@@ -2,33 +2,44 @@ import LOCATORS from "../../support/locators";
 import HomePage from "../Page/HomePage";
 import LoginPage from "../Page/LoginPage";
 
-describe('User Login Test', () => {
-    const homePage = new HomePage
-    const loginPage = new LoginPage
+describe("User Login Test", () => {
+  const homePage = new HomePage();
+  const loginPage = new LoginPage();
 
-    let user;
+  let user;
 
-    before(() => {
-        cy.fixture('userDatas/info').then((userInfo) => {
-            user = userInfo
-        })
-    })
-
-    it('Kullanici login yapabilmelli', () => {
-        homePage.visitPage()
-        cy.title().should('eq', user.home.title)
-        cy.getBySel(LOCATORS.HOME_PAGE.LOGIN_BTN).click()
-        cy.getBySel(LOCATORS.LOGIN_PAGE.LOGIN_ACCOUNT_TEXT).should('have.text', user.userLoginPage.loginAccountText)
-        loginPage.userLogin(user)
-        cy.getByCompoundSel(LOCATORS.HOME_PAGE.HEADER, LOCATORS.LOGIN_PAGE.LOGGED_AS_TEXT).should('be.visible')
+  before(() => {
+    cy.fixture("userDatas/info").then((userInfo) => {
+      user = userInfo;
     });
-    it("Kullanici yanliş email ve password ile giriş yaptiginda  uyari mesaji alabilmeli", () => {
-        user.userLoginPage.emailAddress = "testlogin@mail.com"
-        homePage.visitPage()
-        cy.getBySel(LOCATORS.HOME_PAGE.LOGIN_BTN).click()
-        cy.getBySel(LOCATORS.LOGIN_PAGE.LOGIN_ACCOUNT_TEXT).should('have.text', user.userLoginPage.loginAccountText)
-        loginPage.userLogin(user)
-        cy.getBySel(LOCATORS.LOGIN_PAGE.LOGIN_ERROR_MESSAGE).should('have.text', user.loginPage.errorText)
+  });
 
-    })
-}); 
+  it("User should be able to login with correct email and password", () => {
+    homePage.visitPage();
+    cy.title().should("eq", user.home.title);
+    cy.getBySel(LOCATORS.HOME_PAGE.LOGIN_BTN).click();
+    cy.getBySel(LOCATORS.LOGIN_PAGE.LOGIN_ACCOUNT_TEXT).should(
+      "have.text",
+      user.userLoginPage.loginAccountText
+    );
+    loginPage.userLogin(user);
+    cy.getByCompoundSel(
+      LOCATORS.HOME_PAGE.HEADER,
+      LOCATORS.LOGIN_PAGE.LOGGED_AS_TEXT
+    ).should("be.visible");
+  });
+  it("The user should receive a warning message when logging with the wrong email and password", () => {
+    user.userLoginPage.emailAddress = "testlogin@mail.com";
+    homePage.visitPage();
+    cy.getBySel(LOCATORS.HOME_PAGE.LOGIN_BTN).click();
+    cy.getBySel(LOCATORS.LOGIN_PAGE.LOGIN_ACCOUNT_TEXT).should(
+      "have.text",
+      user.userLoginPage.loginAccountText
+    );
+    loginPage.userLogin(user);
+    cy.getBySel(LOCATORS.LOGIN_PAGE.LOGIN_ERROR_MESSAGE).should(
+      "have.text",
+      user.loginPage.errorText
+    );
+  });
+});
--- cypress/e2e/Test/LogoutUserTest.cy.js
+++ cypress/e2e/Test/LogoutUserTest.cy.js
@@ -1,23 +1,23 @@
-import HomePage from "../Page/HomePage"
-import LoginPage from "../Page/LoginPage"
-import LOCATORS from "../../support/locators"
-describe('Kullanici siteye kaydini sildiginde giris sayfasina geri gelebilmeli',()=>{
-    const homPage = new HomePage()
-    const lginPage = new LoginPage()
-    let user;
-    before(() => {
-        cy.fixture('userDatas/info').then((userInfo) => {
-            user = userInfo
-        })
-    })
-    it('Kullanici siteye kaydini sildiginde giris sayfasina geri gelebilmeli',()=>{
-homPage.visitPage()
-cy.getBySel(LOCATORS.HOME_PAGE.HEADER).should('be.visible')
-cy.getBySel(LOCATORS.HOME_PAGE.LOGIN_BTN).click()
-cy.getBySel(LOCATORS.LOGIN_PAGE.LOGIN_ACCOUNT_TEXT).should('be.visible')
-lginPage.userLogin(user)
-cy.getBySel(LOCATORS.LOGIN_PAGE.LOGGED_AS_TEXT).should('be.visible')
-cy.getBySel(LOCATORS.HOME_PAGE.LOGOUT_BTN).click()
-cy.getBySel(LOCATORS.LOGIN_PAGE.LOGIN_ACCOUNT_TEXT).should('be.visible')
-     })
-}) 
+import HomePage from "../Page/HomePage";
+import LoginPage from "../Page/LoginPage";
+import LOCATORS from "../../support/locators";
+describe("Logout User", () => {
+  const homPage = new HomePage();
+  const loginPage = new LoginPage();
+  let user;
+  before(() => {
+    cy.fixture("userDatas/info").then((userInfo) => {
+      user = userInfo;
+    });
+  });
+  it("The user should be able to return to the login page when they unregister from the site", () => {
+    homPage.visitPage();
+    cy.getBySel(LOCATORS.HOME_PAGE.HEADER).should("be.visible");
+    cy.getBySel(LOCATORS.HOME_PAGE.LOGIN_BTN).click();
+    cy.getBySel(LOCATORS.LOGIN_PAGE.LOGIN_ACCOUNT_TEXT).should("be.visible");
+    loginPage.userLogin(user);
+    cy.getBySel(LOCATORS.LOGIN_PAGE.LOGGED_AS_TEXT).should("be.visible");
+    cy.getBySel(LOCATORS.HOME_PAGE.LOGOUT_BTN).click();
+    cy.getBySel(LOCATORS.LOGIN_PAGE.LOGIN_ACCOUNT_TEXT).should("be.visible");
+  });
+});
